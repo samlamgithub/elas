1e21d2e37216f06a7b4e578c0a5a61084ca83908
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2017-02-12 17:16:29 +0000

    add 3d recon lib

--- projects/ElasticFusion/jni/Android.mk
+++ projects/ElasticFusion/jni/Android.mk
@@ -7,7 +7,9 @@ LOCAL_MODULE           := elasticfusion
 
 LOCAL_SHARED_LIBRARIES := tango_client_api \
                           ar_toolkit \
+                          tango_3d_reconstruction \
                           tango_support_api
+LOCAL_STATIC_LIBRARIES := png
 LOCAL_CFLAGS           := -Werror -std=c++11  -O0 -g -D__ANDROID__
 
 LOCAL_SRC_FILES := point_cloud_drawable.cc \
@@ -21,9 +23,11 @@ LOCAL_SRC_FILES := point_cloud_drawable.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/frustum.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/gesture_camera.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/grid.cc \
+                   $(PROJECT_ROOT_FROM_JNI)/tango_gl/tango_gl.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/line.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/shaders.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/trace.cc \
+                   $(PROJECT_ROOT_FROM_JNI)/tango_gl/texture.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/transform.cc \
                    $(PROJECT_ROOT_FROM_JNI)/tango_gl/util.cc
            
@@ -37,5 +41,7 @@ LOCAL_CC	             := /usr/bin/gcc-4.8
 include $(BUILD_SHARED_LIBRARY)
 $(call import-add-path, $(LOCAL_PATH)/../..)
 $(call import-module, tango_client_api)
+$(call import-module,libpng)
 $(call import-module, ar_toolkit)
+$(call import-module,tango_3d_reconstruction)
 $(call import-module, tango_support_api)
